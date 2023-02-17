1d379b6adaaa7ac56fb03234bb76dbe77941d6e2
ki-boop <karpakovkirill@gmail.com>
2023-02-17 23:36:58 +0400

    bearing-info in second screen

--- front/src/app/shared/shared.module.ts
+++ front/src/app/shared/shared.module.ts
@@ -3,11 +3,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { AngularSvgIconModule } from 'angular-svg-icon';
 import { PanelModule } from 'primeng/panel';
+import { BaeringInfoComponent } from './compoents/bearing-info/baering-info.component';
+import { PropertyBearingComponent } from './compoents/bearing-info/property-bearing/property-bearing.component';
 import { ImageComponent } from './compoents/form/image/image.component';
 import { MachineInfoComponent } from './compoents/machine-info/machine-info.component';
 
 @NgModule({
-  declarations: [ImageComponent, MachineInfoComponent],
+  declarations: [
+    ImageComponent,
+    MachineInfoComponent,
+    BaeringInfoComponent,
+    PropertyBearingComponent,
+  ],
   imports: [
     CommonModule,
     RouterModule,
@@ -15,6 +22,6 @@ import { MachineInfoComponent } from './compoents/machine-info/machine-info.comp
     AngularSvgIconModule,
     PanelModule,
   ],
-  exports: [ImageComponent, MachineInfoComponent],
+  exports: [ImageComponent, MachineInfoComponent, BaeringInfoComponent],
 })
 export class SharedModule {}
