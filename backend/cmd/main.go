d6690cc3d14012d69d6999d72210aa690045d56c
Nail Uspanov <uspanov@aeroidea.ru>
2023-02-18 10:06:14 +0400

    commit basic logic

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -2,23 +2,27 @@ package main
 
 import (
 	"fmt"
-	"github.com/sirupsen/logrus"
-	"milkazone/backend/internal/adapters/postgres"
-	"milkazone/backend/internal/infrastructure/env"
+	"milkazone/internal/adapters/postgres"
+	"milkazone/internal/domain/usecase"
+	"milkazone/internal/infrastructure/env"
 )
 
 func main() {
 
 	//соединение с бд
-	_, err := postgres.NewDatabase(fmt.Sprintf("host=%s port=%s dbname=%s user=%s sslmode=%s password=%s sslrootcert=%s",
+	db, err := postgres.NewDatabase(fmt.Sprintf("host=%s port=%s dbname=%s user=%s sslmode=%s password=%s sslrootcert=%s",
 		env.DbHost, env.DbPort, env.DbName, env.DbUser, env.DbSslMode, env.DbPassword, env.DbSslCertPath))
 	if err != nil {
+		fmt.Println(err.Error())
 		return
 	}
 
-	if err != nil {
-		logrus.Fatalf("failed to initialize db %s", err.Error())
-	}
+	//if err != nil {
+	//	logrus.Fatalf("failed to initialize db %s", err.Error())
+	//}
+	fmt.Println("YOU GAY")
+	usecase := usecase.NewExcausterUseCase(*postgres.NewRepository(db))
+	usecase.Process(nil)
 
 	////прокидываю инстанс бд и создаю репозитории
 	//repos := repository.NewRepository(db)
